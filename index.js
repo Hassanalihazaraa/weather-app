71467514355169af47d028d76aa79d715a81f98f
Hassan <hassanalihazaraa@gmail.com>
2020-06-29 18:21:00 +0200

    more commit

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 //import api key
 import {API_KEY} from './config.js';
 //search input
-import {input, location, temperature, humidity} from './util.js';
+import {input, location, temperature, humidity, icon, displayWeather} from './util.js';
 
 //handling input event and fetching data
 const handleChange = e => {
-    e.preventDefault();
+
     const inputValue = e.target.value;
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${API_KEY}&units=metric`)
         .then(res => res.json())
         .then(data => {
             console.log(data)
+            displayWeather.style.visibility = "visible";
             location.textContent = data.name + ', ' + data.sys.country;
-            temperature.textContent = Math.floor(data.main.temp) + ' C';
-            humidity.textContent = data.main.humidity + '%';
+            temperature.textContent = 'Temperature ' + Math.floor(data.main.temp) + ' C';
+            humidity.textContent = 'Humidity ' + data.main.humidity + '%';
+
         })
+    e.preventDefault();
+}
+
+//icons
+const icons = (icon, iconID) => {
+    const skycons = new Skycons();
+    const weatherIcon = icon.replace(/-/g, "_")
 }
 
 //input change event
--- util.js
+++ util.js
@@ -1,4 +1,7 @@
 export const input = document.querySelector("#search");
+export const displayWeather = document.querySelector(".display-weather");
 export const location = document.querySelector(".city");
 export const temperature = document.querySelector(".temp");
 export const humidity = document.querySelector(".humidity");
+export const icon = document.querySelector(".icon");
+
