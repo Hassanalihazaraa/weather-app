c252f006c35ccfde0aacfc1114939a36c92cc49a
Hassan <hassanalihazaraa@gmail.com>
2020-06-29 22:25:17 +0200

    changed promises to aync and await

--- index.js
+++ index.js
@@ -4,20 +4,16 @@ import {API_KEY} from './config.js';
 import {input, location, temperature, humidity, icon, displayWeather} from './util.js';
 
 //handling input event and fetching data
-const handleChange = e => {
-
-    const inputValue = e.target.value;
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${API_KEY}&units=metric`)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-            displayWeather.style.visibility = "visible";
-            location.textContent = data.name + ', ' + data.sys.country;
-            temperature.textContent = 'Temperature ' + Math.floor(data.main.temp) + ' C';
-            humidity.textContent = 'Humidity ' + data.main.humidity + '%';
-
-        })
+const handleChange = async e => {
     e.preventDefault();
+    const inputValue = e.target.value;
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${API_KEY}&units=metric`)
+    const data = await response.json();
+    console.log(data);
+    displayWeather.style.visibility = "visible";
+    location.textContent = data.name + ', ' + data.sys.country;
+    temperature.textContent = 'Temperature ' + Math.floor(data.main.temp) + ' C';
+    humidity.textContent = 'Humidity ' + data.main.humidity + '%';
 }
 
 //icons
--- util.js
+++ util.js
@@ -1,5 +1,5 @@
 export const input = document.querySelector("#search");
-export const displayWeather = document.querySelector(".display-weather");
+export const displayWeather = document.querySelector(".display");
 export const location = document.querySelector(".city");
 export const temperature = document.querySelector(".temp");
 export const humidity = document.querySelector(".humidity");
